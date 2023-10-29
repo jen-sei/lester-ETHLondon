01e6f1a39ab9509b99e30e2f293a8a3c52008b7d
Manu <manu1998kj@gmail.com>
2023-10-29 11:06:52 +0000

    ..

--- packages/nextjs/pages/UI/transcation/components/t_pending_approval_list.jsx
+++ packages/nextjs/pages/UI/transcation/components/t_pending_approval_list.jsx
@@ -1,13 +1,10 @@
-import { Address } from "~~/components/scaffold-eth";
 
 export default function TPendingApprovalList() {
-  function handleApproval() {
-    
-  }
-  const address = "0x34aA3F359A9D614239015126635CE7732c18fDF3";
+  function handleApproval() {}
+  const escrowID = "0x34aA3F359A9D614239015126635CE7732c18fDF3";
   return (
     <div className="flex flex-row place-content-between collapse px-5 py-3 shadow-sm ">
-      <Address address={address} />
+      <div className="w-20 text-ellipsis text-sm truncate ">{p_address}</div>
       <div className="flex flex-row ">
         <div class="grid h-10 w-10 flex-grow card bg-base-300 rounded-none place-items-center" onClick={handleApproval}>
           YES
