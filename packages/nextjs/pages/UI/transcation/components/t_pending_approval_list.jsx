228eb022e6f363954e77632be0d71db147e16d5c
Manu <manu1998kj@gmail.com>
2023-10-29 11:58:46 +0000

    transcation changes

--- packages/nextjs/pages/UI/transcation/components/t_pending_approval_list.jsx
+++ packages/nextjs/pages/UI/transcation/components/t_pending_approval_list.jsx
@@ -1,12 +1,33 @@
+export default function TPendingApprovalList({ escrowID, p_amount, p_escrowId, refreshTranscation }) {
+  // const { writeAsync, isLoading, isMining } = useScaffoldContractWrite({
+  //   contractName: "EscrowSingleChain",
+  //   functionName: "confirmPayment",
+  //   args: [p_escrowId],
+  //   // For payable functions
+  //   value: 0, //parseEther(amount),
+  //   // The number of block confirmations to wait for before considering transaction to be confirmed (default : 1).
+  //   blockConfirmations: 1,
+  //   // The callback function to execute when the transaction is confirmed.
+  //   onBlockConfirmation: txnReceipt => {
+  //     console.log("Transaction blockHash", txnReceipt.blockHash);
+  //   },
+  // });
 
-export default function TPendingApprovalList() {
-  function handleApproval() {}
-  const escrowID = "0x34aA3F359A9D614239015126635CE7732c18fDF3";
+  async function handleApproval() {
+    // await writeAsync();
+    // refreshTranscation();
+  }
   return (
     <div className="flex flex-row place-content-between collapse px-5 py-3 shadow-sm ">
-      <div className="w-20 text-ellipsis text-sm truncate ">{p_address}</div>
+      <div className="flex flex-col flex-1 font-light text-sm">
+        <div className="w-20 text-ellipsis text-sm truncate ">{escrowID}</div>
+        <div className="w-20 text-ellipsis text-sm  ">{p_amount} waiting for confirmation</div>
+      </div>
       <div className="flex flex-row ">
-        <div class="grid h-10 w-10 flex-grow card bg-base-300 rounded-none place-items-center" onClick={handleApproval}>
+        <div
+          class="grid h-10 w-10 flex-grow card bg-base-300 rounded-none place-items-center"
+          onClick={refreshTranscation}
+        >
           YES
         </div>
         <div class="divider divider-horizontal p-0  text-xs font-thin"></div>
